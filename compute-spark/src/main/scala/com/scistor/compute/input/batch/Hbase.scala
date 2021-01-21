d5ab0c629ec0f8db045ef01329bf58608c782e9b
cl1226 <1061788688@qq.com>
2021-01-21 14:17:00 +0800

    update

--- compute-spark/src/main/scala/com/scistor/compute/input/batch/Hbase.scala
+++ compute-spark/src/main/scala/com/scistor/compute/input/batch/Hbase.scala
@@ -2,6 +2,7 @@ package com.scistor.compute.input.batch
 
 import com.scistor.compute.apis.BaseStaticInput
 import com.scistor.compute.model.remote.TransStepDTO
+import org.apache.hadoop.hbase.{HBaseConfiguration, HConstants}
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 class Hbase extends BaseStaticInput {
@@ -32,12 +33,13 @@ class Hbase extends BaseStaticInput {
    **/
   override def getDataset(spark: SparkSession): Dataset[Row] = {
 
-//    val config = HBaseConfiguration.create()
-//    config.set(HConstants.ZOOKEEPER_QUORUM, source.connection_url.split(":")(0))
-//    config.set(HConstants.ZOOKEEPER_CLIENT_PORT, source.connection_url.split(":")(1))
-//    config.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.INPUT_TABLE, source.table_name)
-    spark.read.format("jdbc")
-      .option("driver", "org.apache.phoenix.jdbc.PhoenixDriver")
+    val attrs = config.getStepAttributes
+
+    val hbaseConfig = HBaseConfiguration.create()
+//    hbaseConfig.set(HConstants.ZOOKEEPER_QUORUM, attrs.getOrDefault("serverAddress", "").toString)
+//    hbaseConfig.set(HConstants.ZOOKEEPER_CLIENT_PORT, attrs.getOrDefault("port", "").toString)
+//    hbaseConfig.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.INPUT_TABLE, source.table_name)
+
     var df: Dataset[Row] = null
     df
   }
